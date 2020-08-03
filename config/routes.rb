b24a7b19545691019145f3a009ad31e0bf268d70
Laurent Saives <etrenjeu@gmail.com>
2020-08-04 01:10:57 +0200

    Ajout de la version quasi finalisée

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  root to: 'gossips#home'
+  get '/team', to: 'static#team'
+  get '/contact', to: 'static#contact'
+  get '/welcome/:name', to: 'static#welcome'
+
+  get '/gossip/:id', to: 'gossips#show', as: 'show'
+
+  get '/user/:id', to: 'users#show', as: 'user'
+
+  # get 'gossips/new' ... plus tard...
+
 end
